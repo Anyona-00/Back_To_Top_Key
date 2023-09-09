d29e6efdc14c00d574ce932057549c43c46b9da0
Anyona-00 <125492744+Anyona-00@users.noreply.github.com>
2023-09-09 18:57:26 +0300

    testing js

---
+++ topkey.js
@@ -0,0 +1,44 @@
+
+//declare variables needed
+//calculate the document height and set the offset to a quter of that value
+// add event listener for click and scroll
+var btt = document.getElementById("back-to-top"),
+    body = document.body,
+    docElem = document.documentElement,
+    offset = 100,
+    scrollPos,
+    docHeight,
+   // isFirefox = typeof InstallTrigger !== 'undefined';
+
+    
+
+
+//calculate the document height
+//Math.max() function  is used to find the maximum value among a set of numbers. It accepts any number of arguments and returns the largest value.
+//call variable that i had declared
+
+docHeight = Math.max(body.scrollHeight, body.offsetHeight, docElem.clientHeight, docElem.scrollHeight, docElem.offsetHeight);
+if (docHeight != 'undefined') {
+    offset = docHeight / 4;
+}
+
+//add scroll event listener
+//window.addEventListener(): Allows you to attach event listeners to respond to various events such as mouse clicks, keyboard inputs, or page load.
+
+window.addEventListener( "scroll", function( event ) {
+    scrollPos = body.scrollTop || docElem.scrollTop;
+
+    btt.className = (scrollPos > offset ) ? "visible" : "";
+
+});
+
+//add click listener
+//btt.addEventListener("click", function(event){
+    //event.preventDefault();
+
+   /* if ( isFirefox ) {
+        docElem.scrollTop = 0;
+    }else{
+        body.scrollTop = 0;
+    }
+}); */
